eaf229051ce180c3e7f02015486a4fb4e881e9f0
Ethan Lebowitz <duckunreal@gmail.com>
2022-12-06 11:48:22 -0700

    Made date automatic

--- students/models.py
+++ students/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Student(models.Model):
     name = models.CharField(max_length=200)
-    date = models.DateTimeField()
+    date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
