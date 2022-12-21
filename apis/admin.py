76428e8bf4a3dc7b83be14aa3e80083b5bf8dc15
Tony <tonygovernmentemail@gmail.com>
2022-12-20 22:26:58 -0700

    Added StudentClass to admin page

--- apis/admin.py
+++ apis/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Assessment, Group, Student, StudentScore, GroupScore
+from .models import Assessment, Group, Student, StudentScore, GroupScore,StudentClass
 
-admin.site.register([Assessment, Group, Student, StudentScore, GroupScore])
+admin.site.register([Assessment, Group, Student, StudentScore, GroupScore,StudentClass])
