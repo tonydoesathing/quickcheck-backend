7d12a136a9f485080a7bd65c37977b457e895235
Ethan Lebowitz <duckunreal@gmail.com>
2023-02-26 16:41:08 -0700

    Switched to knox auth

--- auth/urls.py
+++ auth/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from . import views
-from rest_framework.authtoken import views as authviews
+# from . import views
+# from rest_framework.authtoken import views as authviews
 
-urlpatterns=[
-    path('api-token-auth/', authviews.obtain_auth_token),
-    path('users/', views.UserList.as_view()),
-    path('users/<int:pk>/', views.UserDetail.as_view()),
+from knox import views as knox_views
+from auth.views import LoginView
+
+urlpatterns = [
+     path('login/', LoginView.as_view(), name='knox_login'),
+     path('logout/', knox_views.LogoutView.as_view(), name='knox_logout'),
+     path('logoutall/', knox_views.LogoutAllView.as_view(), name='knox_logoutall'),
 ]
--- quickcheck/settings.py
+++ quickcheck/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_extensions',
     'rest_framework',
-    'rest_framework.authtoken',
+    'knox',
     'apis',
     'corsheaders',
 ]
@@ -147,13 +147,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+        'knox.auth.TokenAuthentication',
     ],
-    # 'DEFAULT_PERMISSION_CLASSES': [
-    #     'rest_framework.permissions.IsAuthenticated',
-    # ]
 }
 
 CSRF_COOKIE_SECURE = True
 
 SESSION_COOKIE_SECURE = True
+
+REST_KNOX = {
+  'TOKEN_TTL': None,
+}
