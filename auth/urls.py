85a62a53365fd94c8d45a4552c13f4e001440c33
Ethan Lebowitz <duckunreal@gmail.com>
2023-03-16 16:45:43 -0600

    Changed login endpoint name back to api-token-auth

--- auth/urls.py
+++ auth/urls.py
@@ -6,7 +6,7 @@ from knox import views as knox_views
 from auth.views import LoginView
 
 urlpatterns = [
-     path('login/', LoginView.as_view(), name='knox_login'),
+     path('api-token-auth/', LoginView.as_view(), name='knox_login'),
      path('logout/', knox_views.LogoutView.as_view(), name='knox_logout'),
      path('logoutall/', knox_views.LogoutAllView.as_view(), name='knox_logoutall'),
 ]
